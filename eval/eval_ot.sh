d9950950b95e60cf2f41dd6e83898e894e49711c
Helw150 <wbh230@nyu.edu>
2023-04-21 14:04:28 -0400

    Eval Script for OT Version

---
+++ eval/eval_ot.sh
@@ -0,0 +1,14 @@
+export CUDA_VISIBLE_DEVICES=$(nvidia-smi --query-gpu=memory.free,index --format=csv,nounits,noheader | sort -nr | head -n 1 | awk '{ print $NF }')
+export CUDA_VISIBLE_DEVICES=$(echo $CUDA_VISIBLE_DEVICES | tr " " ",")
+
+CKPT=$(ls -t /data/wheld3/models/ot_biencoder_ECB/checkpoint-*/pytorch_model.bin | tail -n 1 | awk '{ print $NF }')
+echo $CKPT
+
+DS=$1
+
+python eval_biencoder.py \
+       --model_name_or_path "Intel/ColBERT-NQ" \
+       --cache_dir "/data/.cache" \
+       --dataset $DS \
+       --load_path $CKPT \
+      
